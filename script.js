67ddc821c615ea5d81ff4e515f953e23d115bbf4
borodos <stepllerrus78@gmail.com>
2022-03-15 15:17:47 +0300

    5

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ const keyboard = document.querySelector(".calculator-keyboard");
 let number = [];
 let buffer = [];
 let resultNumber;
-let result;
+let result = 0;
 let flag = false;
 let displayBufferLastIndex = displayBuffer.innerText.length - 1;
+
+let displayBufferText = [];
 
 // Вешаем прослушку для каждого элемента
 for (const item of numbers) {
@@ -30,52 +32,52 @@ function addNumber(e) {
         switch (e.target.classList[1]) {
             case "key-1":
                 displayMonitor.innerText += "1";
-                displayBuffer.innerText += "1";
+                // displayBuffer.innerText += "1";
                 number.push(1);
                 break;
             case "key-2":
                 displayMonitor.innerText += "2";
-                displayBuffer.innerText += "2";
+                // displayBuffer.innerText += "2";
                 number.push(2);
                 break;
             case "key-3":
                 displayMonitor.innerText += "3";
-                displayBuffer.innerText += "3";
+                // displayBuffer.innerText += "3";
                 number.push(3);
                 break;
             case "key-4":
                 displayMonitor.innerText += "4";
-                displayBuffer.innerText += "4";
+                // displayBuffer.innerText += "4";
                 number.push(4);
                 break;
             case "key-5":
                 displayMonitor.innerText += "5";
-                displayBuffer.innerText += "5";
+                // displayBuffer.innerText += "5";
                 number.push(5);
                 break;
             case "key-6":
                 displayMonitor.innerText += "6";
-                displayBuffer.innerText += "6";
+                // displayBuffer.innerText += "6";
                 number.push(6);
                 break;
             case "key-7":
                 displayMonitor.innerText += "7";
-                displayBuffer.innerText += "7";
+                // displayBuffer.innerText += "7";
                 number.push(7);
                 break;
             case "key-8":
                 displayMonitor.innerText += "8";
-                displayBuffer.innerText += "8";
+                // displayBuffer.innerText += "8";
                 number.push(8);
                 break;
             case "key-9":
                 displayMonitor.innerText += "9";
-                displayBuffer.innerText += "9";
+                // displayBuffer.innerText += "9";
                 number.push(9);
                 break;
             case "key-0":
                 displayMonitor.innerText += "0";
-                displayBuffer.innerText += "0";
+                // displayBuffer.innerText += "0";
                 number.push(0);
                 break;
 
@@ -86,52 +88,52 @@ function addNumber(e) {
         switch (e.target.classList[1]) {
             case "key-1":
                 displayMonitor.innerText += "1";
-                displayBuffer.innerText += "1";
+                // displayBuffer.innerText += "1";
                 number.push(1);
                 break;
             case "key-2":
                 displayMonitor.innerText += "2";
-                displayBuffer.innerText += "2";
+                // displayBuffer.innerText += "2";
                 number.push(2);
                 break;
             case "key-3":
                 displayMonitor.innerText += "3";
-                displayBuffer.innerText += "3";
+                // displayBuffer.innerText += "3";
                 number.push(3);
                 break;
             case "key-4":
                 displayMonitor.innerText += "4";
-                displayBuffer.innerText += "4";
+                // displayBuffer.innerText += "4";
                 number.push(4);
                 break;
             case "key-5":
                 displayMonitor.innerText += "5";
-                displayBuffer.innerText += "5";
+                // displayBuffer.innerText += "5";
                 number.push(5);
                 break;
             case "key-6":
                 displayMonitor.innerText += "6";
-                displayBuffer.innerText += "6";
+                // displayBuffer.innerText += "6";
                 number.push(6);
                 break;
             case "key-7":
                 displayMonitor.innerText += "7";
-                displayBuffer.innerText += "7";
+                // displayBuffer.innerText += "7";
                 number.push(7);
                 break;
             case "key-8":
                 displayMonitor.innerText += "8";
-                displayBuffer.innerText += "8";
+                // displayBuffer.innerText += "8";
                 number.push(8);
                 break;
             case "key-9":
                 displayMonitor.innerText += "9";
-                displayBuffer.innerText += "9";
+                // displayBuffer.innerText += "9";
                 number.push(9);
                 break;
             case "key-0":
                 displayMonitor.innerText += "0";
-                displayBuffer.innerText += "0";
+                // displayBuffer.innerText += "0";
                 number.push(0);
                 break;
 
@@ -139,29 +141,27 @@ function addNumber(e) {
                 break;
         }
     }
-    displayBufferLastIndex = displayBuffer.innerText.length - 1;
-    console.log(displayBufferLastIndex);
+    // displayBufferLastIndex = displayBuffer.innerText.length - 1;
     resultNumber = +number.join("");
 }
 
 function addOperation(e) {
+    displayBufferText.push(resultNumber);
     flag = true; // Метка, показывающая, нажат ли бы один из операторов. Для добавления следующего числа
-    buffer.push(resultNumber);
-    resultNumber = 0;
+    // buffer.push(resultNumber);
+    // result = resultNumber;
+    // resultNumber = 0;
     number = [];
     switch (e.target.classList[1]) {
         case "plus":
-            if (Number(displayBuffer.innerText[displayBufferLastIndex])) {
-                console.log('+=')
-                displayBuffer.innerText += "+";
-            } else if (displayBuffer.innerText[displayBufferLastIndex] != "+") {
-                console.log('=')
-                displayBuffer.innerText = displayBuffer.innerText.replace(displayBuffer.innerText[displayBufferLastIndex], '+');
-            }
-            displayBufferLastIndex = displayBuffer.innerText.length - 1;
-            result = buffer.reduce((box, element) => {
-                return (box += element);
-            });
+            // result = buffer.reduce((box, element) => {
+            //     return (box += element);
+            // });
+            result += resultNumber;
+            resultNumber = 0;
+            displayBuffer.innerText = '';
+            displayBuffer.innerText = result;
+            displayBuffer.innerText += '+';
             break;
 
         case "clear":
@@ -173,9 +173,6 @@ function addOperation(e) {
             break;
 
         case "root":
-            if (displayBufferText[displayBufferText.length - 1] != "&#8730x") {
-                displayBufferText += "&#8730x";
-            }
             break;
 
         case "share":
@@ -199,26 +196,24 @@ function addOperation(e) {
             break;
 
         case "minus":
-            if (Number(displayBuffer.innerText[displayBufferLastIndex])) {
-                displayBuffer.innerText += "-";
-            } else if (displayBuffer.innerText[displayBufferLastIndex] != "-") {
-                displayBuffer.innerText = displayBuffer.innerText.replace(displayBuffer.innerText[displayBufferLastIndex], '-');
-            }
-            displayBufferLastIndex = displayBuffer.innerText.length - 1;
-
-            result = buffer.reduce((box, element) => {
-                return (box -= element);
-            });
+            // result = buffer.reduce((box, element) => {
+            //     return (box -= element);
+            // });
+            result -= resultNumber;
+            resultNumber = 0;
+            displayBuffer.innerText = '';
+            displayBuffer.innerText = result;
+            displayBuffer.innerText += '-';
             break;
 
         case "equal":
-
-
+            displayBuffer.innerText = '';
+            displayMonitor.innerText = result;
             break;
 
         default:
             break;
     }
-    // displayBuffer.innerText = displayBufferText.join('');
     displayMonitor.innerText = result;
+    buffer.push(result);
 }
